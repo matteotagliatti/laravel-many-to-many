d7075d66d719c0f02723d6ad80a0da3a28d08e54
matteotagliatti <matteotagliatti@gmail.com>
2022-05-24 19:30:37 +0200

    edit / route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index');
+Route::get('/', function() {
+    return view('home');
+});
+
 Route::get('/home', 'HomeController@index');
 
 Route::prefix('posts')
